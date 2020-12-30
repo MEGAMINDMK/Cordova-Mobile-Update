7950eb9f3a03a8424d7b67c7e9a5205be7b415f5
webnet <muhommadraahim1@gmail.com>
2020-12-30 15:30:18 +0500

    Add files via upload

---
+++ www/js/index.js
@@ -0,0 +1,31 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        addCheckAppUpdateInfo();
+    }
+};
+
+function addCheckAppUpdateInfo() {
+    $('version').innerHTML = AppVersion.build;
+}
+
+function checkAppUpdate() {
+   var updateUrl = "https://webnetappupdater.netlify.app/version.xml";
+    window.AppUpdate.checkAppUpdate(onSuccess, onFail, updateUrl);
+    function onFail() {console.log('fail', JSON.stringify(arguments), arguments);}
+    function onSuccess() {
+        console.log('success', JSON.stringify(arguments), arguments);
+    }
+}
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+app.initialize();
+
